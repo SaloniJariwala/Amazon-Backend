b7a1ccc3640c4024612f6f6465ae91f1547ebaff
SaloniJariwala <salonijariwala.developing@gmail.com>
2022-09-02 15:20:48 +0530

    seller module

---
+++ Model/category.js
@@ -0,0 +1,14 @@
+const { default: mongoose } = require("mongoose");
+
+const categorySchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model('categories',)
